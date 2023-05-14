7665c7a3b514ed4ded95cc6fb457838104dc602d
Sofya02 <nikolaenko.sofya@mail.ru>
2023-05-14 17:35:43 +0300

    Commit 4 to main

--- lab6.cpp
+++ lab6.cpp
@@ -21,7 +21,7 @@ int main() {
         cout << "Roots are real and different." << endl;
         cout << "Root 1 = " << root1 << endl;
         cout << "Root 2 = " << root2 << endl;
-		//main 4:
+		//main 4: change 24
     } else if (discriminant == 0) {
         root1 = -b / (2 * a);
 	/////////////////second area/////////////
